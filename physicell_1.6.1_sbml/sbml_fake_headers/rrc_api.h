0c5007083b7cead661ae0466133a4715a0f29a9f
Furkan Kurtoglu <fkurtog@iu.edu>
2020-02-26 18:22:45 -0500

    PhysiCell w/ libroadrunner - initial commit

---
+++ physicell_1.6.1_sbml/sbml_fake_headers/rrc_api.h
@@ -0,0 +1,10 @@
+#if defined(__cplusplus)
+namespace rrc { extern "C" {
+#endif
+
+typedef void* RRHandle; /*! Void pointer to a RoadRunner instance */
+
+#if defined( __cplusplus)
+}
+}//namespace
+#endif
